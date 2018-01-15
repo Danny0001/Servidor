3fcf0b45a68aa301634314771251de80e84b4dca
Daniel Herrera Mena <daniel.herreram@utem.cl>
2018-01-15 16:40:20 -0300

    actualizacion index

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
+
 const express = require('express')
 const app = express()
+const path = require('path')
 
+app.use('/Public', express.static(path.join(__dirname, 'Public')))
 app.get('/', (req, res) => res.send('Hello World!'))
-
+app.get('/Encendido', ( req, res ) => {
+  res.send('Esta Encendida')
+})
+app.put('/Apagado', (req, res) => {
+  res.send('Esta Apagado')
+})
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
