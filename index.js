95ee4e4dd19729f253d3d44a505ef01502d41f9f
Daniel Herrera Mena <daniel.herreram@utem.cl>
2018-01-15 17:16:38 -0300

    Actualizacion

--- index.js
+++ index.js
@@ -5,8 +5,11 @@ const path = require('path')
 let estado= 'Apagado'
 
 app.use('/Public', express.static(path.join(__dirname, 'Public')))
-app.get('/', (req, res) =>  res.sendfile(path.join(__dirname+'/html Apagado.html')))
+app.get('/', (req, res) =>  {
+  estado='Apagado'
+  res.sendfile(path.join(__dirname+'/html Apagado.html'))})
 app.get('/Encender', ( req, res ) => {
+  estado='Encender'
   res.sendFile(path.join (__dirname+'/Html Encedido.html'))
   console.log(estado)
 })
