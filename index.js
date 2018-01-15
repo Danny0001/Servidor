6de8b8d039b931e2a26327bc37127255e460789b
Daniela Ramirez <Daniela.ramirez003@gmail.com>
2018-01-15 17:09:31 -0300

    last

--- index.js
+++ index.js
@@ -2,13 +2,18 @@
 const express = require('express')
 const app = express()
 const path = require('path')
+let estado= 'Apagado'
 
 app.use('/Public', express.static(path.join(__dirname, 'Public')))
-app.get('/', (req, res) => res.send('Hello World!'))
-app.get('/Encendido', ( req, res ) => {
+app.get('/', (req, res) =>  res.sendfile(path.join(__dirname+'/html Apagado.html')))
+app.get('/Encender', ( req, res ) => {
   res.sendFile(path.join (__dirname+'/Html Encedido.html'))
 })
-app.get('/Apagado', (req, res) => {
+app.get('/Apagar', (req, res) => {
+  estado= 'Apagado'
   res.sendfile(path.join(__dirname+'/html Apagado.html'))
+  console.log(estado)
 })
+
+
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
